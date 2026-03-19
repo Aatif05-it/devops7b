5db5fe844e71392cdc049490776b395c0722fd88
Aatif05-it <att.kha.rt24@dypatil.edu>
2026-03-20 00:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Welcome to Flask CI/CD with Kubernetes!"
+   return "Welcome to Updated Version of Flask CI/CD!"
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000)
